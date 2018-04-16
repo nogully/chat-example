36fa109655c091735f23ec6f9a70304585c9e87c
Nora Gully <nora.gully@gmail.com>
2018-04-16 16:42:56 -0600

    Initial commit

--- index.js
+++ index.js
@@ -6,18 +6,21 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on('connection', socket => {
+  socket.emit('message', `A new user, ${Date.now()}, has connected.`)
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+
+  socket.join('chatroom');
+  io.to('chatroom').emit('Welcome to the chatroom, where it is always 1994');
+
   socket.on('disconnect', msg => {
     io.emit('msg', 'Someone has disconnected');
   });
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
